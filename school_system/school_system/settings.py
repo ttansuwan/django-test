c8bbf60e56f107dfa76b9b8b76dacb22e88ea0fa
t.tansuwan <t.tansuwan@gmail.com>
2022-09-28 21:15:35 +0700

    Refactor: add whitenoise middleware

--- school_system/school_system/settings.py
+++ school_system/school_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t-_nqt)&$!ea2q4wb)86sr56_3w9vkq8*qbijz+f*jmi7+8voj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
