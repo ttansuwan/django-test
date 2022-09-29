27199c9f01b4747255786846c52ee5d0b97370c7
t.tansuwan <t.tansuwan@gmail.com>
2022-09-30 02:40:15 +0700

    Add: adding drf pagination to setting

--- school_system/school_system/settings.py
+++ school_system/school_system/settings.py
@@ -154,7 +154,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[],
-    'DEFAULT_PERMISSION_CLASSES':[]
+    'DEFAULT_PERMISSION_CLASSES':[],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
 }
 
 django_heroku.settings(locals())
