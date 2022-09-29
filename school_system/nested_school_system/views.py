6b48fcfc70706ab454940b1943a6183b8a4d0130
t.tansuwan <t.tansuwan@gmail.com>
2022-09-30 02:17:01 +0700

    Add: adding allowany to view

--- school_system/nested_school_system/views.py
+++ school_system/nested_school_system/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from school_report.models import Student, School
+from rest_framework.permissions import AllowAny
 from nested_school_system.serializers import StudentSerializer
 
 # Create your views here.
